37915920bd700f5b8e617178b84715f3a27eebbe
Christian R. Picone <ch.picone@gmail.com>
2025-10-07 21:11:06 +0200

    reset password

--- src/public/js/login/password_recovery.js
+++ src/public/js/login/password_recovery.js
@@ -12,21 +12,30 @@ function passwordrecoveryInit(options={}){
         });
     }
 
-    window.onSubmitReset = function(){
-
-        alert(options.labels.reset_success);
-        window.location = "/frontend/";
-        /*
-        var url = "/backend/auth/password_recovery";
+    window.onSubmitReset = function(){    
+        var pwd1 = document.getElementById("password").value
+        var pwd2 = document.getElementById("re_password").value
+        if(pwd1 !== pwd2){
+            alert(options.labels.different_password);
+            return;
+        }
+        if(pwd1.length < 6){
+            alert(options.labels.invalid_password);
+            return;            
+        }
+        var url = "/backend/users/reset-password";
         axios.post(url, {
-            user: document.getElementById("username").value
+            user: {
+                id: options.user,
+                token: options.token,
+                password: pwd1
+            }
         }).then(response => {
-            alert(options.labels.email_sent);
+            alert(options.labels.reset_success);
             window.location = "/frontend/";
         }).catch(error => {
-            console.log(error);
+            alert(error);
         });
-        */
     }
 
 }
