fc92a3fb3849d0095f225d1e64b9eff176783fb6
Christian R. Picone <ch.picone@gmail.com>
2025-12-21 20:18:39 +0100

    update

---
+++ src/public/js/app/vue-rdf-class-config.js
@@ -0,0 +1,23 @@
+window.classConfig = {
+    'E7_Activity': {
+        'color': 'rgba(174, 93, 56, 0.5)',
+        'label': 'Attività'
+    },
+    'E42_Identifier': {
+        'color': 'rgba(0,0,255,0.5)',
+        'label': 'Identificatore'
+    },
+    'E54_Dimension': {
+        'label':'Dimensione'
+    },
+    'E55_Type': {
+        'color': 'rgba(0,255,0,0.70)', 
+        'label': 'Tipo'
+    },
+    'P90_has_value': {
+        'label': 'Ha valore'
+    },
+    'S13_Sample': {
+        'label': 'Campione'
+    }
+};
