f108a1127b1eee5f14e501f7b6cc3f5954099bd0
Christian R. Picone <219723796+chpigea@users.noreply.github.com>
2025-12-23 08:53:31 +0100

    Add new class configurations for architecture types

--- src/public/js/app/vue-rdf-class-config.js
+++ src/public/js/app/vue-rdf-class-config.js
@@ -1,23 +1,642 @@
 window.classConfig = {
-    'E7_Activity': {
-        'color': 'rgba(174, 93, 56, 0.5)',
-        'label': 'Attività'
+    "CP35_Building-Formal_Type": {
+        "color": "#FAB565",
+        "label_en": "Building-Formal Type",
+        "label_it": "Tipo formale dell’edificio"
     },
-    'E42_Identifier': {
-        'color': 'rgba(0,0,255,0.5)',
-        'label': 'Identificatore'
+    "CP36_Architecture_Type": {
+        "color": "#FAB565",
+        "label_en": "Architecture Type",
+        "label_it": "Tipo architettonico"
     },
-    'E54_Dimension': {
-        'label':'Dimensione'
+    "CP37_Architecture_Graphic_Representation_Type": {
+        "color": "#FAB565",
+        "label_en": "Architecture Graphic Representation Type",
+        "label_it": "Tipo di rappresentazione grafica architettonica"
     },
-    'E55_Type': {
-        'color': 'rgba(0,255,0,0.70)', 
-        'label': 'Tipo'
+    "CP39_Architecture_Alpha_Numerical_Representation_Type": {
+        "color": "#FAB565",
+        "label_en": "Architecture Alpha Numerical Representation Type",
+        "label_it": "Tipo di rappresentazione alfanumerica architettonica"
     },
-    'P90_has_value': {
-        'label': 'Ha valore'
+    "CP47_Decoration_Type": {
+        "color": "#FAB565",
+        "label_en": "Decoration Type",
+        "label_it": "Tipo di decorazione"
     },
-    'S13_Sample': {
-        'label': 'Campione'
+    "CP48_Equipment_Type": {
+        "color": "#FAB565",
+        "label_en": "Equipment Type",
+        "label_it": "Tipo di attrezzatura"
+    },
+    "E55_Type": {
+        "color": "#FAB565",
+        "label_en": "Type",
+        "label_it": "Tipo"
+    },
+    "E56_Language": {
+        "color": "#FAB565",
+        "label_en": "Language",
+        "label_it": "Lingua"
+    },
+    "E57_Material": {
+        "color": "#FAB565",
+        "label_en": "Material",
+        "label_it": "Materiale"
+    },
+    "E58_Measurement_Unit": {
+        "color": "#FAB565",
+        "label_en": "Measurement Unit",
+        "label_it": "Unità di misura"
+    },
+    "CP23_Maintenance": {
+        "color": "#82DDFF",
+        "label_en": "Maintenance",
+        "label_it": "Manutenzione"
+    },
+    "CP24_Architecture_Conservation_Project_Activity": {
+        "color": "#82DDFF",
+        "label_en": "Architecture Conservation Project Activity",
+        "label_it": "Attività di progetto di conservazione architettonica"
+    },
+    "CP25_Conservation_Intervention": {
+        "color": "#82DDFF",
+        "label_en": "Conservation Intervention",
+        "label_it": "Intervento di conservazione"
+    },
+    "CP26_Typological_Variation": {
+        "color": "#82DDFF",
+        "label_en": "Typological Variation",
+        "label_it": "Variazione tipologica"
+    },
+    "CP27_Architecture_Analysis_Output": {
+        "color": "#82DDFF",
+        "label_en": "Architecture Analysis Output",
+        "label_it": "Risultato dell’analisi architettonica"
+    },
+    "CP28_Building_Feature": {
+        "color": "#82DDFF",
+        "label_en": "Building Feature",
+        "label_it": "Caratteristica dell’edificio"
+    },
+    "CP29_Building_Phase": {
+        "color": "#82DDFF",
+        "label_en": "Building Phase",
+        "label_it": "Fase dell’edificio"
+    },
+    "CP30_Architecture_Condition_Assessment": {
+        "color": "#82DDFF",
+        "label_en": "Architecture Condition Assessment",
+        "label_it": "Valutazione dello stato di conservazione architettonica"
+    },
+    "CP31_Mechanical_Damage_Assessment": {
+        "color": "#82DDFF",
+        "label_en": "Mechanical Damage Assessment",
+        "label_it": "Valutazione del danno meccanico"
+    },
+    "CP32_Architecture_Features_Analysis": {
+        "color": "#82DDFF",
+        "label_en": "Architecture Features Analysis",
+        "label_it": "Analisi delle caratteristiche architettoniche"
+    },
+    "CP42_Material_Decay": {
+        "color": "#82DDFF",
+        "label_en": "Material Decay",
+        "label_it": "Degrado del materiale"
+    },
+    "CP43_Structural_Damage": {
+        "color": "#82DDFF",
+        "label_en": "Structural Damage",
+        "label_it": "Danno strutturale"
+    },
+    "CP46_Building_Activity": {
+        "color": "#82DDFF",
+        "label_en": "Building Activity",
+        "label_it": "Attività edilizia"
+    },
+    "E11_Modification": {
+        "color": "#82DDFF",
+        "label_en": "Modification",
+        "label_it": "Modifica"
+    },
+    "E12_Production": {
+        "color": "#82DDFF",
+        "label_en": "Production",
+        "label_it": "Produzione"
+    },
+    "E13_Attribute_Assignment": {
+        "color": "#82DDFF",
+        "label_en": "Attribute Assignment",
+        "label_it": "Assegnazione di attributo"
+    },
+    "E14_Condition_Assessment": {
+        "color": "#82DDFF",
+        "label_en": "Condition Assessment",
+        "label_it": "Valutazione dello stato"
+    },
+    "E15_Identifier_Assignment": {
+        "color": "#82DDFF",
+        "label_en": "Identifier Assignment",
+        "label_it": "Assegnazione di identificatore"
+    },
+    "E16_Measurement": {
+        "color": "#82DDFF",
+        "label_en": "Measurement",
+        "label_it": "Misurazione"
+    },
+    "E17_Type_Assignment": {
+        "color": "#82DDFF",
+        "label_en": "Type Assignment",
+        "label_it": "Assegnazione di tipo"
+    },
+    "E2_Temporal_Entity": {
+        "color": "#82DDFF",
+        "label_en": "Temporal Entity",
+        "label_it": "Entità temporale"
+    },
+    "E3_Condition_State": {
+        "color": "#82DDFF",
+        "label_en": "Condition State",
+        "label_it": "Stato di conservazione"
+    },
+    "E4_Period": {
+        "color": "#82DDFF",
+        "label_en": "Period",
+        "label_it": "Periodo"
+    },
+    "E5_Event": {
+        "color": "#82DDFF",
+        "label_en": "Event",
+        "label_it": "Evento"
+    },
+    "E6_Destruction": {
+        "color": "#82DDFF",
+        "label_en": "Destruction",
+        "label_it": "Distruzione"
+    },
+    "E63_Beginning_of_Existence": {
+        "color": "#82DDFF",
+        "label_en": "Beginning of Existence",
+        "label_it": "Inizio dell’esistenza"
+    },
+    "E64_End_of_Existence": {
+        "color": "#82DDFF",
+        "label_en": "End of Existence",
+        "label_it": "Fine dell’esistenza"
+    },
+    "E65_Creation": {
+        "color": "#82DDFF",
+        "label_en": "Creation",
+        "label_it": "Creazione"
+    },
+    "E66_Formation": {
+        "color": "#82DDFF",
+        "label_en": "Formation",
+        "label_it": "Formazione"
+    },
+    "E67_Birth": {
+        "color": "#82DDFF",
+        "label_en": "Birth",
+        "label_it": "Nascita"
+    },
+    "E68_Dissolution": {
+        "color": "#82DDFF",
+        "label_en": "Dissolution",
+        "label_it": "Dissoluzione"
+    },
+    "E7_Activity": {
+        "color": "#82DDFF",
+        "label_en": "Activity",
+        "label_it": "Attività"
+    },
+    "E79_Part_Addition": {
+        "color": "#82DDFF",
+        "label_en": "Part Addition",
+        "label_it": "Aggiunta di parte"
+    },
+    "E80_Part_Removal": {
+        "color": "#82DDFF",
+        "label_en": "Part Removal",
+        "label_it": "Rimozione di parte"
+    },
+    "E81_Transformation": {
+        "color": "#82DDFF",
+        "label_en": "Transformation",
+        "label_it": "Trasformazione"
+    },
+    "I12_Adopted_Belief": {
+        "color": "#82DDFF",
+        "label_en": "Adopted Belief",
+        "label_it": "Credenza adottata"
+    },
+    "I2_Belief": {
+        "color": "#82DDFF",
+        "label_en": "Belief",
+        "label_it": "Credenza"
+    },
+    "I5_Inference_Making": {
+        "color": "#82DDFF",
+        "label_en": "Inference Making",
+        "label_it": "Formulazione di inferenza"
+    },
+    "S1_Matter_Removal": {
+        "color": "#82DDFF",
+        "label_en": "Matter Removal",
+        "label_it": "Rimozione di materia"
+    },
+    "S2_Sample_Taking": {
+        "color": "#82DDFF",
+        "label_en": "Sample Taking",
+        "label_it": "Prelievo di campione"
+    },
+    "S4_Observation": {
+        "color": "#82DDFF",
+        "label_en": "Observation",
+        "label_it": "Osservazione"
+    },
+    "S5_Inference_Making": {
+        "color": "#82DDFF",
+        "label_en": "Inference Making",
+        "label_it": "Formulazione di inferenza"
+    },
+    "CP41_Environmental_Observable_Entity": {
+        "color": "#FFFFFF",
+        "label_en": "Environmental Observable Entity",
+        "label_it": "Entità ambientale osservabile"
+    },
+    "E1_CRM_Entity": {
+        "color": "#FFFFFF",
+        "label_en": "CRM Entity",
+        "label_it": "Entità CRM"
+    },
+    "E70_Thing": {
+        "color": "#FFFFFF",
+        "label_en": "Thing",
+        "label_it": "Cosa"
+    },
+    "E72_Legal_Object": {
+        "color": "#FFFFFF",
+        "label_en": "Legal Object",
+        "label_it": "Oggetto giuridico"
+    },
+    "E77_Persistent_Item": {
+        "color": "#FFFFFF",
+        "label_en": "Persistent Item",
+        "label_it": "Elemento persistente"
+    },
+    "S10_Material_Substantial": {
+        "color": "#FFFFFF",
+        "label_en": "Material Substantial",
+        "label_it": "Sostanza materiale"
+    },
+    "S11_Amount_of_Matter": {
+        "color": "#FFFFFF",
+        "label_en": "Amount of Matter",
+        "label_it": "Quantità di materia"
+    },
+    "S13_Sample": {
+        "color": "#FFFFFF",
+        "label_en": "Sample",
+        "label_it": "Campione"
+    },
+    "E41_Appellation": {
+        "color": "#FEF3BA",
+        "label_en": "Appellation",
+        "label_it": "Denominazione"
+    },
+    "E42_Identifier": {
+        "color": "#FEF3BA",
+        "label_en": "Identifier",
+        "label_it": "Identificatore"
+    },
+    "CP33_Architecture_Conservation_Project": {
+        "color": "#FDDC34",
+        "label_en": "Architecture Conservation Project",
+        "label_it": "Progetto di conservazione architettonica"
+    },
+    "CP34_Architecture_Depiction": {
+        "color": "#FDDC34",
+        "label_en": "Architecture Depiction",
+        "label_it": "Rappresentazione architettonica"
+    },
+    "CP38_Architecture_Representation_Object": {
+        "color": "#FDDC34",
+        "label_en": "Architecture Representation Object",
+        "label_it": "Oggetto di rappresentazione architettonica"
+    },
+    "CP45_Architecture_Project": {
+        "color": "#FDDC34",
+        "label_en": "Architecture Project",
+        "label_it": "Progetto architettonico"
+    },
+    "CP49_Urban_Plan": {
+        "color": "#FDDC34",
+        "label_en": "Urban Plan",
+        "label_it": "Piano urbanistico"
+    },
+    "CP50_Urban_Conservation_Plan": {
+        "color": "#FDDC34",
+        "label_en": "Urban Conservation Plan",
+        "label_it": "Piano di conservazione urbana"
+    },
+    "E28_Conceptual_Object": {
+        "color": "#FDDC34",
+        "label_en": "Conceptual Object",
+        "label_it": "Oggetto concettuale"
+    },
+    "E29_Design_or_Procedure": {
+        "color": "#FDDC34",
+        "label_en": "Design or Procedure",
+        "label_it": "Progetto o procedura"
+    },
+    "E30_Right": {
+        "color": "#FDDC34",
+        "label_en": "Right",
+        "label_it": "Diritto"
+    },
+    "E31_Document": {
+        "color": "#FDDC34",
+        "label_en": "Document",
+        "label_it": "Documento"
+    },
+    "E32_Authority_Document": {
+        "color": "#FDDC34",
+        "label_en": "Authority Document",
+        "label_it": "Documento autoritativo"
+    },
+    "E33_Linguistic_Object": {
+        "color": "#FDDC34",
+        "label_en": "Linguistic Object",
+        "label_it": "Oggetto linguistico"
+    },
+    "E34_Inscription": {
+        "color": "#FDDC34",
+        "label_en": "Inscription",
+        "label_it": "Iscrizione"
+    },
+    "E35_Title": {
+        "color": "#FDDC34",
+        "label_en": "Title",
+        "label_it": "Titolo"
+    },
+    "E36_Visual_Item": {
+        "color": "#FDDC34",
+        "label_en": "Visual Item",
+        "label_it": "Oggetto visivo"
+    },
+    "E37_Mark": {
+        "color": "#FDDC34",
+        "label_en": "Mark",
+        "label_it": "Segno"
+    },
+    "E73_Information_Object": {
+        "color": "#FDDC34",
+        "label_en": "Information Object",
+        "label_it": "Oggetto informativo"
+    },
+    "E78_Curated_Holding": {
+        "color": "#FDDC34",
+        "label_en": "Curated Holding",
+        "label_it": "Collezione curata"
+    },
+    "E89_Propositional_Object": {
+        "color": "#FDDC34",
+        "label_en": "Propositional Object",
+        "label_it": "Oggetto proposizionale"
+    },
+    "E90_Symbolic_Object": {
+        "color": "#FDDC34",
+        "label_en": "Symbolic Object",
+        "label_it": "Oggetto simbolico"
+    },
+    "I4_Proposition_Set": {
+        "color": "#FDDC34",
+        "label_en": "Proposition Set",
+        "label_it": "Insieme di proposizioni"
+    },
+    "E59_Primitive_Value": {
+        "color": "#F0F0F0",
+        "label_en": "Primitive Value",
+        "label_it": "Valore primitivo"
+    },
+    "E61_Time_Primitive": {
+        "color": "#F0F0F0",
+        "label_en": "Time Primitive",
+        "label_it": "Primitivo temporale"
+    },
+    "E62_String": {
+        "color": "#F0F0F0",
+        "label_en": "String",
+        "label_it": "Stringa"
+    },
+    "E94_Space_Primitive": {
+        "color": "#F0F0F0",
+        "label_en": "Space Primitive",
+        "label_it": "Primitivo spaziale"
+    },
+    "E95_Spacetime_Primitive": {
+        "color": "#F0F0F0",
+        "label_en": "Spacetime Primitive",
+        "label_it": "Primitivo spazio-temporale"
+    },
+    "E54_Dimension": {
+        "color": "#B8B8B8",
+        "label_en": "Dimension",
+        "label_it": "Dimensione"
+    },
+    "CP1_Built_Entity": {
+        "color": "#C78E66",
+        "label_en": "Built Entity",
+        "label_it": "Entità costruita"
+    },
+    "CP10_Building_Unit": {
+        "color": "#C78E66",
+        "label_en": "Building Unit",
+        "label_it": "Unità edilizia"
+    },
+    "CP11_Building_Front": {
+        "color": "#C78E66",
+        "label_en": "Building Front",
+        "label_it": "Fronte dell’edificio"
+    },
+    "CP12_Building_Floor": {
+        "color": "#C78E66",
+        "label_en": "Building Floor",
+        "label_it": "Piano dell’edificio"
+    },
+    "CP13_Urban_Unit": {
+        "color": "#C78E66",
+        "label_en": "Urban Unit",
+        "label_it": "Unità urbana"
+    },
+    "CP14_Urban_Unit_Front": {
+        "color": "#C78E66",
+        "label_en": "Urban Unit Front",
+        "label_it": "Fronte dell’unità urbana"
+    },
+    "CP15_Open_Air_Area": {
+        "color": "#C78E66",
+        "label_en": "Open Air Area",
+        "label_it": "Area a cielo aperto"
+    },
+    "CP16_Urban_Area": {
+        "color": "#C78E66",
+        "label_en": "Urban Area",
+        "label_it": "Area urbana"
+    },
+    "CP17_Cultural_Heritage_Landscape_Element": {
+        "color": "#C78E66",
+        "label_en": "Cultural Heritage Landscape Element",
+        "label_it": "Elemento del paesaggio culturale"
+    },
+    "CP2_Architecture_Work": {
+        "color": "#C78E66",
+        "label_en": "Architecture Work",
+        "label_it": "Opera architettonica"
+    },
+    "CP20_Construction_Work": {
+        "color": "#C78E66",
+        "label_en": "Construction Work",
+        "label_it": "Opera di costruzione"
+    },
+    "CP3_Construction_Unit": {
+        "color": "#C78E66",
+        "label_en": "Construction Unit",
+        "label_it": "Unità di costruzione"
+    },
+    "CP4_Construction_Component": {
+        "color": "#C78E66",
+        "label_en": "Construction Component",
+        "label_it": "Componente costruttivo"
+    },
+    "CP40_Historic_Centre": {
+        "color": "#C78E66",
+        "label_en": "Historic Centre",
+        "label_it": "Centro storico"
+    },
+    "CP44_Construction_Site": {
+        "color": "#C78E66",
+        "label_en": "Construction Site",
+        "label_it": "Cantiere"
+    },
+    "CP5_Construction_Element_Plural": {
+        "color": "#C78E66",
+        "label_en": "Construction Element Plural",
+        "label_it": "Elementi costruttivi plurali"
+    },
+    "CP51_Building_Component": {
+        "color": "#C78E66",
+        "label_en": "Building Component",
+        "label_it": "Componente edilizio"
+    },
+    "CP6_Construction_Element_Singular": {
+        "color": "#C78E66",
+        "label_en": "Construction Element Singular",
+        "label_it": "Elemento costruttivo singolare"
+    },
+    "CP7_Architecture_Decoration": {
+        "color": "#C78E66",
+        "label_en": "Architecture Decoration",
+        "label_it": "Decorazione architettonica"
+    },
+    "CP8_Equipment": {
+        "color": "#C78E66",
+        "label_en": "Equipment",
+        "label_it": "Attrezzatura"
+    },
+    "CP9_Building_Material": {
+        "color": "#C78E66",
+        "label_en": "Building Material",
+        "label_it": "Materiale edilizio"
+    },
+    "E18_Physical_Thing": {
+        "color": "#C78E66",
+        "label_en": "Physical Thing",
+        "label_it": "Cosa fisica"
+    },
+    "E19_Physical_Object": {
+        "color": "#C78E66",
+        "label_en": "Physical Object",
+        "label_it": "Oggetto fisico"
+    },
+    "E21_Person": {
+        "color": "#C78E66",
+        "label_en": "Person",
+        "label_it": "Persona"
+    },
+    "E22_Human-Made_Object": {
+        "color": "#C78E66",
+        "label_en": "Human-Made Object",
+        "label_it": "Oggetto prodotto dall’uomo"
+    },
+    "E24_Physical_Human-Made_Thing": {
+        "color": "#C78E66",
+        "label_en": "Physical Human-Made Thing",
+        "label_it": "Cosa fisica prodotta dall’uomo"
+    },
+    "E25_Human-Made_Feature": {
+        "color": "#C78E66",
+        "label_en": "Human-Made Feature",
+        "label_it": "Caratteristica prodotta dall’uomo"
+    },
+    "E26_Physical_Feature": {
+        "color": "#C78E66",
+        "label_en": "Physical Feature",
+        "label_it": "Caratteristica fisica"
+    },
+    "E27_Site": {
+        "color": "#C78E66",
+        "label_en": "Site",
+        "label_it": "Sito"
+    },
+    "S22_Segment_of_Matter": {
+        "color": "#C78E66",
+        "label_en": "Segment of Matter",
+        "label_it": "Segmento di materia"
+    },
+    "E39_Actor": {
+        "color": "#FFBDCA",
+        "label_en": "Actor",
+        "label_it": "Attore"
+    },
+    "E74_Group": {
+        "color": "#FFBDCA",
+        "label_en": "Group",
+        "label_it": "Gruppo"
+    },
+    "E52_Time-Span": {
+        "color": "#86BBC8",
+        "label_en": "Time-Span",
+        "label_it": "Intervallo temporale"
+    },
+    "E53_Place": {
+        "color": "#94CC7D",
+        "label_en": "Place",
+        "label_it": "Luogo"
+    },
+    "CP18_Space_Entity": {
+        "color": "#CC80FF",
+        "label_en": "Space Entity",
+        "label_it": "Entità spaziale"
+    },
+    "CP19_Historic_Centre": {
+        "color": "#CC80FF",
+        "label_en": "Historic Centre",
+        "label_it": "Centro storico"
+    },
+    "CP21_Space_Unit": {
+        "color": "#CC80FF",
+        "label_en": "Space Unit",
+        "label_it": "Unità spaziale"
+    },
+    "CP22_Space_Component": {
+        "color": "#CC80FF",
+        "label_en": "Space Component",
+        "label_it": "Componente spaziale"
+    },
+    "E92_Spacetime_Volume": {
+        "color": "#CC80FF",
+        "label_en": "Spacetime Volume",
+        "label_it": "Volume spazio-temporale"
     }
 };
